4adb840aa4ea2ce8cb3cc1943588864b638f7e5d
Pete Millspaugh <73900714+pmillspaugh@users.noreply.github.com>
2024-10-14 10:27:15 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,31 +1,16 @@
 import { state, nodes } from "membrane";
 
-/**
- * Click `Invoke ►` above the fn signature,
- * then peep Logs (bottom) and check your email.
- */
+// Click `Invoke ►` above the fn signature, then see Logs (bottom) and check your email
 export async function run() {
   console.log("Hello World");
-
-  // `nodes` is how you connect to your other programs
-  await nodes.email.send({ subject: "Welcome to Membrane", body: "👋" });
+  await nodes.email.send({ subject: "Welcome to Membrane", body: "👋" }); // `nodes` is how you connect to your other programs
 }
 
-/**
- * Click `Open Endpoint URL ↗` above the fn signature,
- * then come back here to see Logs and state (bottom left).
- */
+// Click `Open Endpoint URL ↗`, then come back here to see Logs and state (bottom left)
 export async function endpoint({ method, path, body, headers, query }) {
-  // `state` is like a little db for your program
-  if (!state.hits) state.hits = 0;
+  if (!state.hits) state.hits = 0; // `state` is like a little db for your program
   state.hits++;
-
   return `Visitors: ${state.hits}`;
 }
 
-/**
- * Congrats on your first Membrane program! 🎉
- *
- * Explore docs: https://docs.membrane.io
- * Join our discord: https://discord.gg/4RHyJDV8kj
- */
+// Congrats on your first Membrane program! 🎉
