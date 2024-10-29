aaabaf3dccb2c12988ad9f432494f20b96ccaf52
Pete Millspaugh <73900714+pmillspaugh@users.noreply.github.com>
2024-10-29 18:07:34 -0400

    Update hello world

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
 import { state, nodes } from "membrane";
 
-// Click `Invoke ►` above the fn signature, then see Logs (bottom) and check your email
+// Click `Invoke ►` below, then see Logs (bottom) and check your email
 export async function run() {
   console.log("Hello World");
-  await nodes.email.send({ subject: "Welcome to Membrane", body: "👋" }); // `nodes` is how you connect to your other programs
+
+  // `nodes` is how you connect to your other programs
+  await nodes.email.send({ subject: "Welcome to Membrane", body: "👋" });
 }
 
-// Click `Open Endpoint URL ↗`, then come back here to see Logs and state (bottom left)
+// Click `Open Endpoint URL ↗`, then check Logs and state (bottom left)
 export async function endpoint({ method, path, body, headers, query }) {
-  if (!state.hits) state.hits = 0; // `state` is like a little db for your program
+  // `state` is like a little db for your program
+  if (!state.hits) state.hits = 0;
   state.hits++;
+
   return `Visitors: ${state.hits}`;
 }
