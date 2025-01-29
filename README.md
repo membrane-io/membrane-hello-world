50a5e0545b730129a561a5f2bf02d47af3210c9f
Pete Millspaugh <73900714+pmillspaugh@users.noreply.github.com>
2025-01-29 16:46:53 -0500

    Improve instructions (#2)

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ This program scratches the surface of what you can do with Membrane. Start by op
 
 - Check out [docs](https://docs.membrane.io)
 - Join [our discord](https://discord.gg/4RHyJDV8kj)
-- Explore programs the Membrane team and other users have published (`FIND PROGRAMS`, top right of IDE)
+- Explore programs the Membrane team and other users have published (`PACKAGES`, top left of IDE)
 - [Book a demo](https://cal.com/team/membrane) with us
--- index.ts
+++ index.ts
@@ -1,18 +1,31 @@
+/**
+ * Welcome to Membrane!
+ * 1. This `hello-world` example is your first Membrane program.
+ * 2. You have a few other programs pre-installed: `http`, `email`, and `sms`.
+ * 3. Follow the comments in this file to play with `hello-world`. Save to deploy your changes.
+ * 4. Read through docs to better understand how Membrane works: https://docs.membrane.io
+ * 5. Install our `membrane/getting-started` program for a more in-depth tutorial 
+ *    (click "PACKAGES" in the top left, search getting-started, and install).
+ */
+
 import { state, nodes } from "membrane";
 
-// Click `Invoke ►` above the `run` fn signature, then see Logs (bottom) and check your email
+// `run` is a Membrane action defined by the "SCHEMA" on the right sidebar.
+// Click `Invoke ►` (below this comment) to send yourself a welcome email! (You'll also see it below in Logs).
 export async function run() {
-  console.log("Hello World");
+  console.log("Sending email...");
 
-  // `nodes` is how you connect to your other programs
+  // `nodes` holds connections to other programs (see `email` on the right sidebar under "CONNECTIONS").
   await nodes.email.send({ subject: "Welcome to Membrane", body: "👋" });
 }
 
-// Click `Open Preview` (above fn signature), then check Logs and state (bottom left)
+// `endpoint` is an action that every program has with its own instantly deployed URL.
+// Click `Open Preview` (below this comment), then check Logs (bottom) and "STATE" (right sidebar).
 export async function endpoint({ method, path, body, headers, query }) {
-  // `state` is like a little db for your program
-  if (!state.hits) state.hits = 0;
-  state.hits++;
+  // This program is durable! `state` is like a little database for your program where you can store any JS object.
+  if (!state.hits) {
+    state.hits = 0;
+  }
 
-  return `Visitors: ${state.hits}`;
+  return `Visitors: ${++state.hits}`;
 }
