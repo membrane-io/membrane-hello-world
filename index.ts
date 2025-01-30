0ee31db280c35d5dbdbd1b9fcdb81a0ffa959d17
Pete Millspaugh <73900714+pmillspaugh@users.noreply.github.com>
2025-01-30 15:48:15 -0500

    Move some instructions to product tour (#3)

--- index.ts
+++ index.ts
@@ -1,16 +1,6 @@
-/**
- * Welcome to Membrane!
- * 1. This `hello-world` example is your first Membrane program.
- * 2. You have a few other programs pre-installed: `http`, `email`, and `sms`.
- * 3. Follow the comments in this file to play with `hello-world`. Save to deploy your changes.
- * 4. Read through docs to better understand how Membrane works: https://docs.membrane.io
- * 5. Install our `membrane/getting-started` program for a more in-depth tutorial 
- *    (click "PACKAGES" in the top left, search getting-started, and install).
- */
-
 import { state, nodes } from "membrane";
 
-// `run` is a Membrane action defined by the "SCHEMA" on the right sidebar.
+// `run` is a Membrane action defined in this program's "SCHEMA" on the right sidebar.
 // Click `Invoke ►` (below this comment) to send yourself a welcome email! (You'll also see it below in Logs).
 export async function run() {
   console.log("Sending email...");
@@ -19,7 +9,7 @@ export async function run() {
   await nodes.email.send({ subject: "Welcome to Membrane", body: "👋" });
 }
 
-// `endpoint` is an action that every program has with its own instantly deployed URL.
+// `endpoint` is an action that every Membrane program has, with its own instantly deployed URL.
 // Click `Open Preview` (below this comment), then check Logs (bottom) and "STATE" (right sidebar).
 export async function endpoint({ method, path, body, headers, query }) {
   // This program is durable! `state` is like a little database for your program where you can store any JS object.
@@ -29,3 +19,10 @@ export async function endpoint({ method, path, body, headers, query }) {
 
   return `Visitors: ${++state.hits}`;
 }
+
+// That's just scratching the surface!
+//
+// For a more in-depth tutorial, install our `membrane/getting-started` program.
+//    |--> Click PACKAGES in the top left, search getting-started, then INSTALL.
+//
+// And check out docs to read more about how Membrane works: https://docs.membrane.io
