da0f34f24654634fc6e44b5a95332ae41e449ec6
Pete Millspaugh <73900714+pmillspaugh@users.noreply.github.com>
2025-02-21 14:00:39 -0500

    Edits for welcome tour (#5)

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Hello World (from Membrane)
 
-This program scratches the surface of what you can do with Membrane. Start by opening up [index.ts](./index.ts) and following code comment instructions.
+This program scratches the surface of what you can do with Membrane. Visit [ide.membrane.io?tour=true](https://ide.membrane.io?tour=true) for a walk-through of the `hello-world` program.
 
 ## What's next?
 
 - Check out [docs](https://docs.membrane.io)
 - Join [our discord](https://discord.gg/4RHyJDV8kj)
-- Explore programs the Membrane team and other users have published (`PACKAGES`, top left of IDE)
+- Explore programs the Membrane team and other users have published (`FIND PROGRAMS`, top right of IDE)
 - [Book a demo](https://cal.com/team/membrane) with us
--- index.ts
+++ index.ts
@@ -1,28 +1,15 @@
 import { state, nodes } from "membrane";
 
-// `run` is a Membrane action defined in this program's "SCHEMA" on the right sidebar.
-// Click `Invoke ►` (below this comment) to send yourself a welcome email! (You'll also see it below in Logs).
-export async function run() {
+export async function welcome() {
   console.log("Sending email...");
 
-  // `nodes` holds connections to other programs (see `email` on the right sidebar under "CONNECTIONS").
   await nodes.email.send({ subject: "Welcome to Membrane", body: "👋" });
 }
 
-// `endpoint` is an action that every Membrane program has, with its own instantly deployed URL.
-// Click `Open Preview` (below this comment), then check Logs (bottom) and "STATE" (right sidebar).
 export async function endpoint({ method, path, body, headers, query }) {
-  // This program is durable! `state` is like a little database for your program where you can store any JS object.
   if (!state.hits) {
     state.hits = 0;
   }
 
-  return `Visitors: ${++state.hits}`;
+  return `Page views: ${++state.hits}`;
 }
-
-// That's just scratching the surface!
-//
-// For a more in-depth tutorial, install our `membrane/getting-started` program.
-//    |--> Click PACKAGES in the top left, search getting-started, then INSTALL.
-//
-// And check out docs to read more about how Membrane works: https://docs.membrane.io
